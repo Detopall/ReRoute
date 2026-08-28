fb6e75bdfefd26873d82451fd6ded622fb60ffa6
Detopall <denis.topallaj13@gmail.com>
2026-08-28 18:06:02 +0200

    fix maplibre gl css loading for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-	base: "./",
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": `${import.meta.dirname}/src`,
-		},
-	},
-});
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "./" : "/",
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["maplibre-gl"],
+  },
+  server: {
+    allowedHosts: [
+      'lively-reassured-salamander.loca.lt'
+    ],
+  },
+  resolve: {
+    alias: {
+      "@": `${import.meta.dirname}/src`,
+    },
+  },
+}));
