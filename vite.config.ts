4653297d4bb19832069ed35743b22b9a527b4fe9
Detopall <denis.topallaj13@gmail.com>
2026-08-28 17:39:07 +0200

    modified config files

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "/reroute/",
+	base: "/reroute",
 	plugins: [react()],
 	optimizeDeps: {
 		exclude: ["maplibre-gl"],
